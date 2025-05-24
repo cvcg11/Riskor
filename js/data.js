b1c1a1eeddc7c54fb91c0efe01efcf0d6b917369
Diaz-coding-01 <ed.diaz.hz@gmail.com>
2025-05-24 12:06:32 -0600

    Global class css updated and medical Records

---
+++ js/data.js
@@ -0,0 +1,38 @@
+const registros = [
+    {
+        "DUI": "12345678-9",
+        "Employee": "Edwin Díaz",
+        "BloodType": "A+",
+        "Status": "Active"
+    },
+    {
+        "DUI": "12345678-8",
+        "Employee": "Kevin Castro",
+        "BloodType": "OH+",
+        "Status": "Active"
+    },
+    {
+        "DUI": "12345678-7",
+        "Employee": "Jorge Pérez",
+        "BloodType": "A-",
+        "Status": "Active"
+    },
+    {
+        "DUI": "12345678-9",
+        "Employee": "Edwin Díaz",
+        "BloodType": "A+",
+        "Status": "Active"
+    },
+    {
+        "DUI": "12345678-8",
+        "Employee": "Kevin Castro",
+        "BloodType": "OH+",
+        "Status": "Active"
+    },
+    {
+        "DUI": "12345678-7",
+        "Employee": "Jorge Pérez",
+        "BloodType": "A-",
+        "Status": "Active"
+    }
+];
