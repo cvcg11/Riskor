2a83e90f825707f028d5e79f27773004d2552ed7
KeruChips <imkeru26@gmail.com>
2025-05-26 21:16:49 -0600

    Aplicación del nav en las intefaces y modificacion de Flexbox

--- js/sidebar.js
+++ js/sidebar.js
@@ -7,7 +7,7 @@ nav.innerHTML = `
     <nav class="sidebar">
         <header>
             <div class="superiorNav">
-                    <img src="img/LogoRiskor.png" alt="Logo de Riskor">
+                    <img src="../media/sidebar/LogoRiskor.png" alt="Logo de Riskor">
             </div>
         </header>
         <hr class="hr-sidebar">
@@ -16,61 +16,61 @@ nav.innerHTML = `
                 <div class="menu-link">
                     <li class="nav-link">
                             <a href="#" class="links activeNavbarItem">
-                                <img src="img/Inicio.svg" alt="" class="navbar-icon">
+                                <img src="../media/sidebar/Inicio.svg" alt="" class="navbar-icon">
                                 <span class="text nav-text">Inicio</span>
                             </a> 
                     </li>
                     <li class="nav-link dropdown">
                         <a href="#" class="links dropdown-toggle">
-                                <img src="img/Empleado.svg" alt="" class="navbar-icon" />
+                                <img src="../media/sidebar/Empleado.svg" alt="" class="navbar-icon" />
                                 <span class="text nav-text">Empleados</span>
                         </a>
                     </li>
                     <li class="nav-link">
                             <a href="#" class="links activeNavbarItem">
-                                 <img src="img/Accidentes.svg" alt="" class="navbar-icon">
+                                 <img src="../media/sidebar/Accidentes.svg" alt="" class="navbar-icon">
                             <span class="text nav-text">Accidentes</span>
                             </a> 
                     </li>
                     <li class="nav-link">
                             <a href="#" class="links activeNavbarItem">
-                            <img src="img/capacitaciones.svg" alt="" class="navbar-icon">
+                            <img src="../media/sidebar/capacitaciones.svg" alt="" class="navbar-icon">
                             <span class="text nav-text">Capacitaciones</span>
                             </a> 
                     </li>
                     <li class="nav-link">
                             <a href="#" class="links activeNavbarItem">
-                            <img src="img/permisos.svg" alt="" class="navbar-icon">
+                            <img src="../media/sidebar/permisos.svg" alt="" class="navbar-icon">
                             <span class="text nav-text">Permisos</span>
                             </a> 
                     </li>
                     <li class="nav-link">
                             <a href="#" class="links activeNavbarItem">
-                            <img src="img/Incapacidad.svg" alt="" class="navbar-icon">
+                            <img src="../media/sidebar/Incapacidad.svg" alt="" class="navbar-icon">
                             <span class="text nav-text">Incapacidad</span>
                             </a> 
                     </li>
                     <li class="nav-link">
                             <a href="#" class="links activeNavbarItem">
-                            <img src="img/RegulacionesEmpresariales.svg" alt="" class="nav-icon">
+                            <img src="../media/sidebar/RegulacionesEmpresariales.svg" alt="" class="nav-icon">
                             <span class="text nav-text">Regulaciones...</span>
                             </a> 
                     </li>
                     <li class="nav-link">
                             <a href="#" class="links activeNavbarItem">
-                            <img src="img/AreasEmpresa.svg" alt="" class="nav-icon">
+                            <img src="../media/sidebar/AreasEmpresa.svg" alt="" class="nav-icon">
                             <span class="text nav-text">Areas </span>
                             </a> 
                     </li>
                     <li class="nav-link">
                             <a href="#" class="links activeNavbarItem">
-                            <img src="img/Infraestructura.svg" alt="" class="nav-icon">
+                            <img src="../media/sidebar/Infraestructura.svg" alt="" class="nav-icon">
                             <span class="text nav-text">Dispositivos</span>
                             </a> 
                     </li>
                     <li class="nav-link">
                             <a href="#" class="links activeNavbarItem">
-                            <img src="img/Configuracion.svg" alt="" class="nav-icon">
+                            <img src="../media/sidebar/Configuracion.svg" alt="" class="nav-icon">
                             <span class="text nav-text">Configuracion</span>
                             </a> 
                     </li>
@@ -78,7 +78,7 @@ nav.innerHTML = `
             </div>
         <div class="userContainer">
             <a href="" class="navbarUserItem">
-                <img src="img/Usuario.svg" alt="" class="userIcon">
+                <img src="../media/sidebar/Usuario.svg" alt="" class="userIcon">
                 <span class="navbarText navTextVisible">User</span>
             </a>
         </div>
